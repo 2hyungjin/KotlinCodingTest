7dfa5b95d34bab52c5838ed1c8170694fe606b61
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2021-10-20 23:28:36 +0900

    K번째 수

---
+++ src/level1/IsPrime.kt
@@ -0,0 +1,9 @@
+package level1
+
+fun solution(nums: IntArray): Int {
+    var answer = 0
+    nums.forEach {
+        
+    }
+    return answer
+}
---
+++ src/level1/KNumber.kt
@@ -0,0 +1,20 @@
+package level1
+
+fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
+    var answer = mutableListOf<Int>()
+    commands.forEach {
+        val range = it[0]..it[1]
+        array.slice(range)
+        val temp = array.filterIndexed { index, i ->
+            index + 1 in range
+        }.sorted()
+        println(temp)
+        answer.add(temp.get(it[2] - 1))
+    }
+    return answer.toIntArray()
+}
+
+fun main() {
+    println(solution(intArrayOf(1, 5, 2, 6, 3, 7, 4),
+        arrayOf(intArrayOf(2, 5, 3), intArrayOf(4, 4, 1), intArrayOf(1, 7, 3))))
+}
